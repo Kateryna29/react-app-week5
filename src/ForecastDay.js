866defec93f7f37b3c463e032a4a1671358371b4
Kateryna29 <rinaniky@gmail.com>
2023-01-08 23:08:06 +0100

    Trying to fix app

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -6,17 +6,12 @@ export default function ForecastDay(props) {
   const [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
   function ForecastrResponse(respons) {
-    console.log(respons.data);
     setForecast(respons.data.daily);
     setLoaded(true);
   }
 
   if (loaded) {
-    return (
-      <div className="ForecastInfo">
-        <DateFormatForecast dateForecast={forecast[0]} />
-      </div>
-    );
+    return <DateFormatForecast dateForecast={forecast[0]} />;
   } else {
     const APIkey = "o25a94199tbb4037b02fa17a9ad37fed";
     let ApiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.date.city}&key=${APIkey}&units=metric`;
