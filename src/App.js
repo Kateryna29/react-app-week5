88bde8d0f1e6b0b766f627482931eb023edb5da2
Kateryna29 <rinaniky@gmail.com>
2023-01-08 02:17:42 +0100

    Add API, AXIOS, UseState component

--- src/App.js
+++ src/App.js
@@ -1,147 +1,199 @@
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="d-flex">
-          <div className="p-2 flex-grow-1">
-            <form>
-              <div className="d-flex">
-                <div className="p-2 flex-grow-1">
-                  <input
-                    type="search"
-                    placeholder="Enter a city.."
-                    className="form-control"
-                    autoFocus="on"
-                  />
+  const [weatherInfo, setWeatherInfo] = useState({ ready: false });
+  function WeatherResponse(respons) {
+    console.log(respons.data);
+    setWeatherInfo({
+      ready: true,
+      dataCurrent: "Sunday 01:47",
+      temperature: respons.data.temperature.current,
+      feelslike: respons.data.temperature.feels_like,
+      wind: respons.data.wind.speed,
+      humadity: respons.data.temperature.humidity,
+      description: respons.data.condition.description,
+      iconUrl: respons.data.condition.icon_url,
+      city: respons.data.city,
+    });
+  }
+
+  if (weatherInfo.ready) {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="d-flex">
+            <div className="p-2 flex-grow-1">
+              <form>
+                <div className="d-flex">
+                  <div className="p-2 flex-grow-1">
+                    <input
+                      type="search"
+                      placeholder="Enter a city.."
+                      className="form-control"
+                      autoFocus="on"
+                    />
+                  </div>
+                  <div className="p-2">
+                    <input
+                      type="submit"
+                      value="Search"
+                      className="btn btn-primary w-100"
+                    />
+                  </div>
                 </div>
-                <div className="p-2">
-                  <input
-                    type="submit"
-                    value="Search"
-                    className="btn btn-primary w-100"
-                  />
-                </div>
-              </div>
-            </form>
-          </div>
-          <div className="p-3">
-            <button className="btn btn-primary w-100">Current location</button>
-          </div>
-        </div>
-        <div className="d-flex">
-          <div className="p-2 flex-grow-1">
-            <h1 className="CityName">New York</h1>
-            <p className="DateInfo">Saturday 21:36</p>
-            <p className="DescriptionInfo">Overcast Clouds</p>
-            <div className="row">
-              <div className="col-2">
-                <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png" />
-              </div>
-              <div className="col-2">
-                <span className="TempValue">7</span>°C
-              </div>
+              </form>
             </div>
-            <div className="row">
-              <div className="col">
-                <button className="btn btn-primary w-100">
-                  Detailed information
-                </button>
-              </div>
-              <div className="col">
-                <button className="btn btn-primary w-100">
-                  Hourly information
-                </button>
-              </div>
-              <div className="DetailedInfo">
-                <ul>
-                  <li>Feel like: °C</li>
-                  <li>Humidity: %</li>
-                  <li>Wind: km/h</li>
-                </ul>
-              </div>
+            <div className="p-3">
+              <button className="btn btn-primary w-100">
+                Current location
+              </button>
             </div>
           </div>
-          <div className="p-3">
-            <ul className="DayForecast">
-              <li>
-                <div className="NameDayForecast">Sat</div>
-                <div className="row">
-                  <div className="col">
-                    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" />
-                  </div>
-                  <div className="col">
-                    <div className="row">
-                      <div className="col">7°</div>
-                      <div className="col">0°</div>
+          <div className="d-flex">
+            <div className="p-2 flex-grow-1">
+              <h1 className="CityName">{weatherInfo.city}</h1>
+              <div className="DateInfo">{weatherInfo.dataCurrent}</div>
+              <div className="DescriptionInfo">{weatherInfo.description}</div>
+              <div className="row">
+                <div className="col-5" id="MainIcon">
+                  <img
+                    src={weatherInfo.iconUrl}
+                    alt={weatherInfo.description}
+                  />
+                </div>
+                <div className="col-5">
+                  <span className="TempValue">
+                    {Math.round(weatherInfo.temperature)}
+                  </span>
+                  <span className="metric"> °C</span>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col">
+                  <button className="btn btn-primary w-100">
+                    Detailed information
+                  </button>
+                </div>
+                <div className="col">
+                  <button className="btn btn-primary w-100">
+                    Hourly information
+                  </button>
+                </div>
+                <div className="DetailedInfo">
+                  <ul>
+                    <li>Feels like: {Math.round(weatherInfo.feelslike)}°C</li>
+                    <li>Humidity: {Math.round(weatherInfo.humadity)}%</li>
+                    <li>Wind: {Math.round(weatherInfo.wind)}km/h</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="p-3">
+              <ul className="DayForecast">
+                <li>
+                  <div className="NameDayForecast">Sat</div>
+                  <div className="row">
+                    <div className="col">
+                      <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" />
+                    </div>
+                    <div className="col">
+                      <div className="row">
+                        <div className="col">7°</div>
+                        <div className="col">0°</div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </li>
-              <li>
-                <div className="NameDayForecast">Sat</div>
-                <div className="row">
-                  <div className="col">
-                    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" />
-                  </div>
-                  <div className="col">
-                    <div className="row">
-                      <div className="col">7°</div>
-                      <div className="col">0°</div>
+                </li>
+                <li>
+                  <div className="NameDayForecast">Sat</div>
+                  <div className="row">
+                    <div className="col">
+                      <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" />
+                    </div>
+                    <div className="col">
+                      <div className="row">
+                        <div className="col">7°</div>
+                        <div className="col">0°</div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </li>
-              <li>
-                <div className="NameDayForecast">Sat</div>
-                <div className="row">
-                  <div className="col">
-                    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" />
-                  </div>
-                  <div className="col">
-                    <div className="row">
-                      <div className="col">7°</div>
-                      <div className="col">0°</div>
+                </li>
+                <li>
+                  <div className="NameDayForecast">Sat</div>
+                  <div className="row">
+                    <div className="col">
+                      <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" />
+                    </div>
+                    <div className="col">
+                      <div className="row">
+                        <div className="col">7°</div>
+                        <div className="col">0°</div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </li>
-              <li>
-                <div className="NameDayForecast">Sat</div>
-                <div className="row">
-                  <div className="col">
-                    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" />
-                  </div>
-                  <div className="col">
-                    <div className="row">
-                      <div className="col">7°</div>
-                      <div className="col">0°</div>
+                </li>
+                <li>
+                  <div className="NameDayForecast">Sat</div>
+                  <div className="row">
+                    <div className="col">
+                      <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" />
+                    </div>
+                    <div className="col">
+                      <div className="row">
+                        <div className="col">7°</div>
+                        <div className="col">0°</div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </li>
-              <li>
-                <div className="NameDayForecast">Sat</div>
-                <div className="row">
-                  <div className="col">
-                    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" />
-                  </div>
-                  <div className="col">
-                    <div className="row">
-                      <div className="col">7°</div>
-                      <div className="col">0°</div>
+                </li>
+                <li>
+                  <div className="NameDayForecast">Sat</div>
+                  <div className="row">
+                    <div className="col">
+                      <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" />
+                    </div>
+                    <div className="col">
+                      <div className="row">
+                        <div className="col">7°</div>
+                        <div className="col">0°</div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </li>
-            </ul>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
+        <footer>
+          This project was coded by Kateryna Nykonenko and is{" "}
+          <a
+            href="https://github.com/wecodeschool/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://suspicious-beaver-111c4d.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let city = "Paris";
+    const APIkey = "o25a94199tbb4037b02fa17a9ad37fed";
+    let ApiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${APIkey}&units=metric`;
+    axios.get(ApiUrl).then(WeatherResponse);
+    return <div>Loading...</div>;
+  }
 }
 
 export default App;
