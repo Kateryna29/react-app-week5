eb58c41ba0b66b657a7cebdd0e6dcc6393b6e3c7
Kateryna29 <rinaniky@gmail.com>
2023-01-09 14:09:32 +0100

    Forecast for 5 days

--- src/DateFormatForecast.js
+++ src/DateFormatForecast.js
@@ -9,29 +9,27 @@ export default function DateFormatForecast(props) {
     return days[day];
   }
   return (
-    <ul className="DayForecast">
-      <li>
-        <div className="NameDayForecast">{day()}</div>
-        <div className="row">
-          <div className="col">
-            <img
-              src={props.dateForecast.condition.icon_url}
-              alt={props.dateForecast.condition.description}
-            />
-          </div>
-          <div className="col">
-            <div className="row">
-              <div className="col" style={{fontsize: "16px"}}>
-                {Math.round(props.dateForecast.temperature.maximum)}°
-              </div>
-              <div className="col">
-                {" "}
-                {Math.round(props.dateForecast.temperature.minimum)}°
+          <div>
+          <div className="NameDayForecast">{day()}</div>
+          <div className="row">
+            <div className="col">
+              <img
+                src={props.dateForecast.condition.icon_url}
+                alt={props.dateForecast.condition.description}
+              />
+            </div>
+            <div className="col">
+              <div className="row">
+                <div className="col" style={{ fontsize: "16px" }}>
+                  {Math.round(props.dateForecast.temperature.maximum)}°
+                </div>
+                <div className="col">
+                  {" "}
+                  {Math.round(props.dateForecast.temperature.minimum)}°
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </li>
-    </ul>
+        </div>     
   );
 }
--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -11,7 +11,21 @@ export default function ForecastDay(props) {
   }
 
   if (loaded) {
-    return <DateFormatForecast dateForecast={forecast[0]} />;
+    return (
+      <ul className="DayForecast">
+        {forecast.map(function (dailyforecast, index) {
+          if (index < 5) {
+            return (
+              <li key={index}>
+                <DateFormatForecast dateForecast={dailyforecast} />
+              </li>
+            );
+          } else {
+            return null;
+          }
+        })}
+      </ul>
+    );
   } else {
     const APIkey = "o25a94199tbb4037b02fa17a9ad37fed";
     let ApiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.date.city}&key=${APIkey}&units=metric`;
