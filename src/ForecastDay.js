8f75d3a42ea94fe5d26c700afc05f11efc2be9f4
Kateryna29 <rinaniky@gmail.com>
2023-01-10 02:23:29 +0100

    change key

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -31,7 +31,7 @@ export default function ForecastDay(props) {
       </ul>
     );
   } else {
-    const APIkey = "o25a94199tbb4037b02fa17a9ad37fed";
+    const APIkey = "bd79ao40tde3dec118ca46bc3e6dd55f";
     let ApiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.date.city}&key=${APIkey}&units=metric`;
     axios.get(ApiUrl).then(ForecastrResponse);
     return null;
